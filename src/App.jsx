aad61dbadd08c8b166535269553fcf0a52c5d375
Isaias <isaiascabrera20@gmail.com>
2025-11-26 20:29:11 -0300

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 import PageRoutes from './routes/PagesRoutes'
 import { Nav } from './components/Nav/Nav'
+import { Footer } from './components/Footer/Footer'
 import { CartProvider } from './context/CartContext/CartProvider'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <CartProvider>
         <Nav />
         <PageRoutes />
+        <Footer />
       </CartProvider> 
     </BrowserRouter>
   )
